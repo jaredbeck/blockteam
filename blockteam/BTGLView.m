f5c19b947c43dc0b377a97c3f3994f02f8d935db
Jared Beck <jared@jaredbeck.com>
2014-05-12 20:47:38 -0400

    A more helpful triangle

--- blockteam/BTGLView.m
+++ blockteam/BTGLView.m
@@ -63,9 +63,9 @@ static float const kCameraSpeed = 0.1; // radians
 	[BTColor gold];
 	glBegin(GL_TRIANGLES);
 	{
-		glVertex3f(  0.0,  0.6, 0.0);
-		glVertex3f( -0.2, -0.3, 0.0);
-		glVertex3f(  0.2, -0.3 ,0.0);
+		glVertex3f( 0.0, 1.0, 0.0);
+		glVertex3f(-0.5, 0.0, 0.0);
+		glVertex3f( 0.5, 0.0, 0.0);
 	}
 	glEnd();
 }
